143deca9466e70dc0d0e3ad7bf8de835ab554c53
anerto <wilifrad234@gmail.com>
2025-10-13 18:22:25 -0400

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAVmjagkkWpBgeZziDREPP5dJZWb6CzlSU",
+  authDomain: "pd24-bc64b.firebaseapp.com",
+  databaseURL: "https://pd24-bc64b-default-rtdb.firebaseio.com",
+  projectId: "pd24-bc64b",
+  storageBucket: "pd24-bc64b.firebasestorage.app",
+  messagingSenderId: "372719690699",
+  appId: "1:372719690699:web:846a6067dbd3d2ddb58c4e",
+  measurementId: "G-45B7DD41HW"
+};
+
+// Initialize Firebase
